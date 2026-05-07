5f66ace42dd40ef09213876de06f5147da5609b3
Sithumina0027 <40-bit-0027@kdu.ac.lk>
2026-05-07 20:58:26 +0530

    Fix: Add basename to Router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Footer } from "./Components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio">
       <div className="App">
         <NavBar />
         <Banner />
